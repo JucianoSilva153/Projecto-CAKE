d68ce6754808d5117af1b18a0bd7af7eb91552ab
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2023-07-06 22:45:14 +0100

    add endpoint de nova categoria

---
+++ Projecto Back/Controllers/CategoriaController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projecto_Back.Data;
+using Projecto_Back.Models;
+using Projecto_Front.Context;
+using Projecto_Front.Models;
+
+namespace Projecto_Back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriaController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Categoria/Nova")]
+        public async Task<RetornoDados> NovaCategoria([FromServices] DataContext data, [FromBody] Categoria categoria)
+        {
+            if(categoria is null)
+                return new RetornoDados(){
+                    Entidade = null,
+                    Mensagem = "Insira uma Categoria"
+                };
+            
+            var add = new CategoriasAcessoDados(data);
+            return add.NovaCategoria(categoria);
+        }
+    }
+}
