f08de821c7624e82d873160cf3e61d01301db525
jucianosilva153 <jucianodasilvasilvaa@gmail.com>
2023-07-24 01:20:07 +0100

    primeira parte da UI feita: introducao e menu(quase)

--- Projecto_Front/tailwind.config.js
+++ Projecto_Front/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./**/*.{html,razor}"],
   theme: {
     extend: {},
+    colors: {
+      "Cor-Primaria" : "#FF708A",
+      "Cor-Secundaria" : "#FFFFFF",
+      "Cor-Detalhes01" : "#FF1F48",
+      "Cor-Detalhes02" : "#9F7A81"
+    }
   },
   plugins: [],
 }
