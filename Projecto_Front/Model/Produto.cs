f9eb66697b9cee34a291f8b81abf90b1f444e631
JucianoSilva153 <jucianodasilvasilvaa@gmail.com>
2023-12-08 22:57:42 +0100

    pagina do produto criada

--- Projecto_Front/Model/Produto.cs
+++ Projecto_Front/Model/Produto.cs
@@ -11,16 +11,123 @@ namespace Projecto_Front.Model
         public string? nome { get; set; }
         public string? descricao { get; set; }
         public double preco { get; set; }
-        public string? imagem { get; set; }        
+        public string? imagem { get; set; }
         public bool disponivel { get; set; }
 
 
 
-        public List<Produto> ListarProdutos(){
+        public List<Produto> ListarProdutos()
+        {
             throw new Exception();
         }
 
-        public static List<Produto> ListarProdutosDestaques(){
+        public static List<Produto> ListarProdutosDestaques()
+        {
+            var lista = new List<Produto>();
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Ressois de Peixe",
+                descricao = "Cesto completo 1x20 de ressois crocantes recheados de peixe.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 22000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Ressois de Peixe",
+                descricao = "Cesto completo 1x12 de ressois crocantes recheados de peixe.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 14000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Ressois de Frango",
+                descricao = "Cesto completo 1x12 de ressois crocantes recheados de frango.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 12000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Bolo de Chocolate",
+                descricao = "Bolo de chocolate branco, de 3 camadas para casamentos.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 28000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Bolo de Jimguba",
+                descricao = "Bolo normal de jimguba, perfeito para festas e outros eventos.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 20000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Cochinhas de Frango",
+                descricao = "Tabuleiro de Cochinhas 1x20 completamente recheado de frango.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 15000
+            });
+
+            return lista;
+        }
+
+        public static List<Produto> ListarProdutosBestSellers()
+        {
+            var lista = new List<Produto>();
+
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Bolo de Chocolate",
+                descricao = "Bolo de chocolate branco, de 3 camadas para casamentos.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 28000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Bolo de Jimguba",
+                descricao = "Bolo normal de jimguba, perfeito para festas e outros eventos.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 20000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Cochinhas de Frango",
+                descricao = "Tabuleiro de Cochinhas 1x20 completamente recheado de frango.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 15000
+            });
+            lista.Add(new Produto()
+            {
+                Id = 1,
+                nome = "Cochinhas de Frango",
+                descricao = "Tabuleiro de Cochinhas 1x30 completamente recheado de frango.",
+                disponivel = true,
+                imagem = "SemImagem",
+                preco = 30000
+            });
+
+            return lista;
+        }
+
+        public static List<Produto> PesquisarProdutos(string pesquisa)
+        {
             var lista = new List<Produto>();
             lista.Add(new Produto(){
                 Id = 1,
@@ -71,46 +178,8 @@ namespace Projecto_Front.Model
                 preco = 15000
             });
 
-            return lista;
-        }
 
-        public static List<Produto> ListarProdutosBestSellers(){
-            var lista = new List<Produto>();
-        
-            lista.Add(new Produto(){
-                Id = 1,
-                nome = "Bolo de Chocolate",
-                descricao = "Bolo de chocolate branco, de 3 camadas para casamentos.",
-                disponivel = true,
-                imagem = "SemImagem",
-                preco = 28000
-            });
-            lista.Add(new Produto(){
-                Id = 1,
-                nome = "Bolo de Jimguba",
-                descricao = "Bolo normal de jimguba, perfeito para festas e outros eventos.",
-                disponivel = true,
-                imagem = "SemImagem",
-                preco = 20000
-            });
-            lista.Add(new Produto(){
-                Id = 1,
-                nome = "Cochinhas de Frango",
-                descricao = "Tabuleiro de Cochinhas 1x20 completamente recheado de frango.",
-                disponivel = true,
-                imagem = "SemImagem",
-                preco = 15000
-            });
-            lista.Add(new Produto(){
-                Id = 1,
-                nome = "Cochinhas de Frango",
-                descricao = "Tabuleiro de Cochinhas 1x30 completamente recheado de frango.",
-                disponivel = true,
-                imagem = "SemImagem",
-                preco = 30000
-            });
-
-            return lista;
+            return lista.FindAll(p => p.nome.ToLower().Contains(pesquisa.ToLower()));
         }
 
     }
